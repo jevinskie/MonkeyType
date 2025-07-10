1705c8efaa3a8e441e3fe503b5c1a51aa604a082
Jevin Sweval <jevinsweval@gmail.com>
2025-07-10 18:10:48 -0400

    add @rewriter and TypeRewriterNuevo

--- monkeytype/typing.py
+++ monkeytype/typing.py
@@ -3,6 +3,9 @@
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
+
+from __future__ import annotations
+
 import functools
 import inspect
 import types
@@ -10,8 +13,10 @@ from abc import ABC, abstractmethod
 from collections import defaultdict
 from itertools import chain
 from typing import (
+    TYPE_CHECKING,
     Any,
     Callable,
+    Concatenate,
     DefaultDict,
     Dict,
     Generator,
@@ -19,11 +24,15 @@ from typing import (
     Iterable,
     Iterator,
     List,
+    NamedTuple,
+    ParamSpec,
     Set,
     Tuple,
     Type,
     TypeVar,
     Union,
+    overload,
+    cast,
 )
 
 from typing_extensions import TypedDict
@@ -38,9 +47,105 @@ from monkeytype.compat import (
     types_equal,
 )
 
+if not TYPE_CHECKING:
+    try:
+        from rich import print
+    except ImportError:
+        pass
+
+
 DUMMY_TYPED_DICT_NAME = "DUMMY_NAME"
 DUMMY_REQUIRED_TYPED_DICT_NAME = "REQUIRED_TYPED_DICT_NAME"
 DUMMY_OPTIONAL_TYPED_DICT_NAME = "OPTIONAL_TYPED_DICT_NAME"
+
+if not TYPE_CHECKING:
+    from rich import print
+
+_T = TypeVar("_T")
+_F = TypeVar("_F", bound=Callable[..., Any])
+_P = ParamSpec("_P")
+_R_co = TypeVar("_R_co", covariant=True)
+
+
+class NamePath(NamedTuple):
+    module: str
+    qualname: str
+
+
+class AnnotatedMethodInfo(NamedTuple):
+    namepath: NamePath
+    name: str
+    method: types.MethodType
+
+
+class AnnotatedMethod(Generic[_T, _P, _R_co]):
+    _np: NamePath
+    _n: str
+    _f: Callable[Concatenate[_T, _P], _R_co]
+
+    # FIXME: Need weakref?
+
+    def __init__(
+        self, func: Callable[Concatenate[_T, _P], _R_co], module: str, qualname: str
+    ) -> None:
+        self._np = NamePath(module, qualname)
+        self._f = func
+
+    @overload
+    def __get__(self, obj: None, cls: type[_T], /) -> Callable[Concatenate[_T, _P], _R_co]: ...
+    @overload
+    def __get__(self, obj: _T, cls: type[_T] | None = None, /) -> Callable[_P, _R_co]: ...
+    def __get__(
+        self, obj: _T | None, cls: type[_T] | None = None, /
+    ) -> Callable[Concatenate[_T, _P], _R_co] | Callable[_P, _R_co]:
+        if obj is None:
+            return self._f
+        return cast(Callable[_P, _R_co], self._f.__get__(obj, cls))
+
+    def __set_name__(self, obj: Any, name: str) -> None:
+        self._n = name
+        if obj is None:
+            raise ValueError(f"None obj? {obj}")
+        if not hasattr(obj, "_infos"):
+            setattr(obj, "_infos", {})
+        obj._infos[self._np] = self.as_ntuple()
+
+    def as_ntuple(self) -> AnnotatedMethodInfo:
+        return AnnotatedMethodInfo(self._np, self._n, cast(types.MethodType, self))
+
+
+class rewriter:
+    _mod: str
+    _qn: str
+
+    def __init__(self, module: str, qualname: str) -> None:
+        self._mod = module
+        self._qn = qualname
+
+    def __call__(self, func: _F) -> _F:
+        return cast(_F, AnnotatedMethod(func, self._mod, self._qn))
+
+
+class TypeRewriterNuevo:
+    _infos: dict[NamePath, AnnotatedMethodInfo]
+    _infos_ro: types.MappingProxyType[NamePath, AnnotatedMethodInfo]
+
+    def __init__(self) -> None:
+        self._infos_ro = types.MappingProxyType(self._infos)
+
+    @rewriter("typing", "Union")
+    def fancy(self, a: int, b: int) -> int:
+        print(f"fancy() self: {self} a: {a} b: {b} registry: {self.registry}")
+        return a + b
+
+    @rewriter("pycparser.c_ast", "Union")
+    def mancy(self, a: int, b: int) -> int:
+        print(f"mancy() self: {self} a: {a} b: {b} registry: {self.registry}")
+        return a * b
+
+    @property
+    def registry(self) -> MappingProxyType[NamePath, AnnotatedMethodInfo]:
+        return self._infos_ro
 
 
 # Functions like shrink_types and get_type construct new types at runtime.
