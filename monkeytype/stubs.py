aea3bb5f5c694102b846d306759bfaa429636aab
Jevin Sweval <jevinsweval@gmail.com>
2025-07-12 13:08:42 -0400

    fixed again

--- monkeytype/stubs.py
+++ monkeytype/stubs.py
@@ -379,10 +379,11 @@ class RenderAnnotation(GenericTypeRewriter[str]):
         print(f"RAN.rewrite() typ: {typ}")
         cstr = caller if caller is not None else ""
         rendered = super().rewrite(typ, caller=f"RAN({cstr}).rewrite")
-        #if getattr(typ, "__module__", None) == "typing":
-        #    rendered = rendered.replace("typing.", "")
-        # Temporary hacky workaround for #76 to fix remaining NoneType hints by search-replace
-        #rendered = rendered.replace("NoneType", "None")
+        if isinstance(rendered, str):
+            if getattr(typ, "__module__", None) == "typing":
+               rendered = rendered.replace("typing.", "")
+            # Temporary hacky workaround for #76 to fix remaining NoneType hints by search-replace
+            rendered = rendered.replace("NoneType", "None")
         return rendered
 
 
