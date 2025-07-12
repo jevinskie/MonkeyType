ba4581902aec8866ab3fd7bc64a140111d7e56e5
Jevin Sweval <jevinsweval@gmail.com>
2025-07-12 12:27:34 -0400

    wip

--- tests/test_stubs.py
+++ tests/test_stubs.py
@@ -876,6 +876,7 @@ class TestStubIndexBuilder:
         idxb = StubIndexBuilder('tests', max_typed_dict_size=0)
         idxb.log(CallTrace(untyped_helper, {'x': int, 'y': str}, str))
         sig = Signature.from_callable(untyped_helper)
+        print(f"sig: {sig} untyped_helper: {untyped_helper}")
         sig = sig.replace(
             parameters=[
                 Parameter('x', Parameter.POSITIONAL_OR_KEYWORD, annotation=int),
